5bd372135fbb4fec50f83d87c6fb588171de4347
rlarson20 <richard.rj.larson@gmail.com>
2025-09-30 19:15:21 -0400

    feat: added repl plugin

---
+++ lua/plugins/iron.lua
@@ -0,0 +1,36 @@
+return {
+  'hkupty/iron.nvim',
+  config = function()
+    local iron = require 'iron.core'
+
+    iron.setup {
+      config = {
+        -- Whether a repl should be discarded or not
+        scratch_repl = true,
+        -- Your repl definitions come here
+        repl_definition = {
+          sh = {
+            -- Can be a table or a function that
+            -- returns a table (see below)
+            command = { 'zsh' },
+          },
+        },
+        -- How the repl window will be displayed
+        -- See below for more information
+        repl_open_cmd = require('iron.view').bottom(40),
+      },
+      -- If the highlight is on, you can change how it looks
+      -- For the available options, check nvim_set_hl
+      highlight = {
+        italic = true,
+      },
+      ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+    }
+
+    -- iron also has a list of commands, see :h iron-commands for all available commands
+    vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+    vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+    vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+    vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+  end,
+}
