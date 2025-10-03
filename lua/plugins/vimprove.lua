2245eebd1e0c6735dc1427ca98816ee5b4d03269
rlarson20 <richard.rj.larson@gmail.com>
2025-10-03 19:10:34 -0400

    fix: needed to tweak Vimprove config

--- lua/plugins/vimprove.lua
+++ lua/plugins/vimprove.lua
@@ -2,8 +2,9 @@ return {
   dir = '~/src/Vimprove/nvim-plugin',
   config = function()
     require('vimprove').setup {
-      cli_path = 'uv run ~/src/Vimprove/cli.py',
+      cli_path = 'uv run --directory ~/src/Vimprove/ ~/src/Vimprove/cli.py',
       api_url = 'http://localhost:8000',
+      model = 'anthropic/claude-4.5-sonnet',
     }
   end,
   cmd = { 'Vimprove', 'VimproveContext' },
