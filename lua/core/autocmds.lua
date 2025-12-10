cf7869b51680c2661d122463619d924175f21777
rlarson20 <richard.rj.larson@gmail.com>
2025-12-10 14:23:53 -0500

    feat: add save folds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -20,3 +20,31 @@ au('LspAttach', 'lsp_attach_disable_ruff_hover', {
   end,
   desc = 'Disable ruff hover on LSP attach',
 })
+
+-- I need to go through my config again to get it more idiomatic
+-- right now I just want saved folds
+local view_group = ag 'auto_view'
+vim.api.nvim_create_autocmd({ 'BufWinLeave', 'BufWritePost', 'WinLeave' }, {
+  desc = 'Save view with mkview for real files',
+  group = view_group,
+  callback = function(args)
+    if vim.b[args.buf].view_activated then
+      vim.cmd.mkview { mods = { emsg_silent = true } }
+    end
+  end,
+})
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  desc = 'Try to load file view if available and enable view saving for real files',
+  group = view_group,
+  callback = function(args)
+    if not vim.b[args.buf].view_activated then
+      local filetype = vim.api.nvim_get_option_value('filetype', { buf = args.buf })
+      local buftype = vim.api.nvim_get_option_value('buftype', { buf = args.buf })
+      local ignore_filetypes = { 'gitcommit', 'gitrebase', 'svg', 'hgcommit' }
+      if buftype == '' and filetype and filetype ~= '' and not vim.tbl_contains(ignore_filetypes, filetype) then
+        vim.b[args.buf].view_activated = true
+        vim.cmd.loadview { mods = { emsg_silent = true } }
+      end
+    end
+  end,
+})
