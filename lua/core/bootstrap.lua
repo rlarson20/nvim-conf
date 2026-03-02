3d95cbccf410072cd22cac7dbc873a40dc41ad30
rlarson20 <richard.rj.larson@gmail.com>
2026-03-02 12:26:25 -0500

    feat: enable experimental loader

--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -1,3 +1,5 @@
+vim.loader.enable()
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', 'https://github.com/folke/lazy.nvim', lazypath }
