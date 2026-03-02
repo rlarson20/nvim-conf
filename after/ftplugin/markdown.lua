e1df07fb5b4bb7328ac8bf53d28f88512bd36cc9
rlarson20 <richard.rj.larson@gmail.com>
2026-03-02 13:36:00 -0500

    fix: now can paste markdown links as headers

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -12,15 +12,31 @@ vim.keymap.set('n', '<leader>mh', function()
   vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { prefix })
   vim.cmd 'startinsert!'
 end, { desc = 'Insert Markdown Heading (count = level, e.g. 3<leader>h for ###)' })
+
 vim.keymap.set('n', '<leader>pl', function()
   local clipboard = vim.fn.getreg '+'
   local link_text = '[](' .. clipboard .. ')'
-  vim.cmd 'normal o'
-  local cursor_pos = vim.api.nvim_win_get_cursor(0)
-  local row, col = cursor_pos[1], cursor_pos[2]
-
-  vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { link_text })
-  vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+  local cur_line = vim.api.nvim_get_current_line()
+  local row, col
+  if cur_line:match '^#{1,6} ?$' then
+    -- blank heading: append link inline, position inside []
+    row = vim.api.nvim_win_get_cursor(0)[1]
+    local insert_col = #cur_line
+    -- ensure there's a space between hashes and link
+    if not cur_line:match ' $' then
+      vim.api.nvim_buf_set_text(0, row - 1, insert_col, row - 1, insert_col, { ' ' })
+      insert_col = insert_col + 1
+    end
+    vim.api.nvim_buf_set_text(0, row - 1, insert_col, row - 1, insert_col, { link_text })
+    col = insert_col + 1 -- inside the []
+  else
+    vim.cmd 'normal o'
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    row, col = cursor_pos[1], cursor_pos[2]
+    vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { link_text })
+    col = col + 1 -- inside the []
+  end
+  vim.api.nvim_win_set_cursor(0, { row, col })
   vim.cmd 'startinsert'
 end, { desc = 'Paste Markdown Link and Title It' })
 
