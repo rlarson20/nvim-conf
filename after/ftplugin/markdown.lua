e743a89dbb28be782c893150275736c2d9d9d02a
rlarson20 <richard.rj.larson@gmail.com>
2026-03-02 12:07:53 -0500

    feat: add header keymap

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,6 +1,17 @@
 vim.api.nvim_create_user_command('OT2MDL', ':%! ~/src/bash/onetab_list_to_md_link.sh', {})
 
 vim.keymap.set('n', '<leader>fm', ':OT2MDL<CR>', { desc = 'OneTab -> MD' })
+
+vim.keymap.set('n', '<leader>h', function()
+  local level = vim.v.count1 -- defaults to 1 when no count is given
+  level = math.min(level, 6) -- clamp to valid heading levels
+  local prefix = string.rep('#', level) .. ' '
+  vim.cmd 'normal o'
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+  local row, col = cursor_pos[1], cursor_pos[2]
+  vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { prefix })
+  vim.cmd 'startinsert!'
+end, { desc = 'Insert Markdown Heading (count = level, e.g. 3<leader>h for ###)' })
 vim.keymap.set('n', '<leader>pl', function()
   local clipboard = vim.fn.getreg '+'
   local link_text = '[](' .. clipboard .. ')'
