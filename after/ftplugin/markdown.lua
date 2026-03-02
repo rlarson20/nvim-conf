1c022194113c0328696422ccc2ad8fce5668d586
rlarson20 <richard.rj.larson@gmail.com>
2026-03-02 12:23:47 -0500

    fix: making heading keymap mh instead of h fix: added opts for markdown editing

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_user_command('OT2MDL', ':%! ~/src/bash/onetab_list_to_md_lin
 
 vim.keymap.set('n', '<leader>fm', ':OT2MDL<CR>', { desc = 'OneTab -> MD' })
 
-vim.keymap.set('n', '<leader>h', function()
+vim.keymap.set('n', '<leader>mh', function()
   local level = vim.v.count1 -- defaults to 1 when no count is given
   level = math.min(level, 6) -- clamp to valid heading levels
   local prefix = string.rep('#', level) .. ' '
@@ -23,3 +23,9 @@ vim.keymap.set('n', '<leader>pl', function()
   vim.api.nvim_win_set_cursor(0, { row, col + 1 })
   vim.cmd 'startinsert'
 end, { desc = 'Paste Markdown Link and Title It' })
+
+vim.opt_local.spell = true
+vim.opt_local.spelllang = 'en_us'
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.conceallevel = 2
